4a358a9b4514d7c1d541f3b97a972835af7708dd
WatIzDat <lily3575@hotmail.com>
2024-06-24 14:23:39 -0700

    Initialize fabric template

---
+++ src/client/java/watizdat/rituals/RitualsClient.java
@@ -0,0 +1,10 @@
+package watizdat.rituals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RitualsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}

007079cefade6c8209d2218c6b323c04b82ceb45
WatIzDat <lily3575@hotmail.com>
2024-06-25 18:10:46 -0700

    Set up DevAuth ensuring data persistence works

---
+++ src/main/java/watizdat/rituals/RitualsPreLaunch.java
@@ -0,0 +1,10 @@
+package watizdat.rituals;
+
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+
+public class RitualsPreLaunch implements PreLaunchEntrypoint {
+    @Override
+    public void onPreLaunch() {
+        System.setProperty("devauth.enabled", "false");
+    }
+}

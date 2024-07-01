2b6070a07a86382520aaea1ddb7ea62efbb0c787
WatIzDat <lily3575@hotmail.com>
2024-06-30 18:03:39 -0700

    Delete unused code and file

--- src/main/java/watizdat/rituals/mixin/MobEntityMixin.java
+++ src/main/java/watizdat/rituals/mixin/MobEntityMixin.java
@@ -1,33 +1,15 @@
 package watizdat.rituals.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.fabricmc.fabric.impl.object.builder.FabricEntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.entity.attribute.DefaultAttributeRegistry;
-import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.PassiveEntity;
-import net.minecraft.registry.Registries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MobEntity.class)
 public class MobEntityMixin {
-//    @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/MobEntity;initGoals()V"))
-//    private void rituals$initGoalsWrapper(MobEntity instance, Operation<Void> original) {
-//        if (((MobEntity) (Object) this) instanceof PassiveEntity) {
-//            return;
-//        }
-//
-//        original.call(instance);
-//    }
-
     @Inject(at = @At("RETURN"), method = "createMobAttributes", cancellable = true)
     private static void rituals$createMobAttributes(CallbackInfoReturnable<DefaultAttributeContainer.Builder> info) {
         info.setReturnValue(info.getReturnValue().add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 0d));

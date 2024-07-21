7b63e941b52b282bbdedf3d48ffa058f7b430673
WatIzDat <lily3575@hotmail.com>
2024-07-21 15:08:39 -0700

    Make tags for entity types depending on dimension

--- src/main/java/watizdat/rituals/init/ModTags.java
+++ src/main/java/watizdat/rituals/init/ModTags.java
@@ -4,12 +4,25 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.EntityType;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 import watizdat.rituals.Rituals;
 
 public class ModTags {
     public static final TagKey<Block> NETHER_RITUAL_POLE_NOT_PLACEABLE =
             TagKey.of(RegistryKeys.BLOCK, Rituals.id("nether_ritual_pole_not_placeable"));
 
-    public static final TagKey<EntityType<?>> AQUATIC =
-            TagKey.of(RegistryKeys.ENTITY_TYPE, Rituals.id("aquatic"));
+    public static final TagKey<EntityType<?>> LIVES_IN_OVERWORLD_WATER =
+            TagKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of("c", "lives_in_overworld_water"));
+
+    public static final TagKey<EntityType<?>> LIVES_IN_OVERWORLD_LAND =
+            TagKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of("c", "lives_in_overworld_land"));
+
+    public static final TagKey<EntityType<?>> LIVES_IN_OVERWORLD_FLYING =
+            TagKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of("c", "lives_in_overworld_flying"));
+
+    public static final TagKey<EntityType<?>> LIVES_IN_NETHER =
+            TagKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of("c", "lives_in_nether"));
+
+    public static final TagKey<EntityType<?>> LIVES_IN_END =
+            TagKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of("c", "lives_in_end"));
 }
